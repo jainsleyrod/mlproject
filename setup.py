3f3539081bc7117113aa861e1b1c05ae392929c4
James <rodriguesjames123@gmail.com>
2024-07-01 15:17:33 +0800

    logging and exception

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+#HYPEN_E_DOT = '-e .'
 def get_requirements(file_path: str) -> List[str]:
     '''
     This function returns list of requirments
@@ -10,8 +10,8 @@ def get_requirements(file_path: str) -> List[str]:
     with open(file_path) as f:
         requirements = f.readlines()
         requirements = [req.replace('\n', '') for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        #if HYPEN_E_DOT in requirements:
+        #    requirements.remove(HYPEN_E_DOT)
     return requirements
 
 
